d15406600b81ebcde360e1509e9fd77d7577e87e
S.Agalya <agalyaishere@gmail.com>
2024-12-29 12:50:01 +0530

    commit

--- src/test/java/com/example/ArraySumTest.java
+++ src/test/java/com/example/ArraySumTest.java
@@ -1,15 +1,18 @@
 package com.example;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 public class ArraySumTest {
+
     @Test
-    void testArraySum(){
-        ArraySum arraysum=new ArraySum();
-      int[] arr={1,2,3,4,5};
-      int res=arraysum.arraySum(arr, arr.length);
-      assertEquals(15,res);
+    public void testArraySum() {
+        ArraySum arraysum = new ArraySum();
+        int[] arr = {1, 2, 3, 4, 5};
+        int res = arraysum.arraySum(arr, arr.length);
+
+        // Corrected assertion
+        Assertions.assertEquals(15, res, "The sum of the array elements is incorrect.");
     }
 }
 
